b5919b235edaa375af5af94cfc5798a19cfc8c93
Ross Freeman <refreeman13@gmail.com>
2014-04-20 15:02:55 -0400

    Update Gemfile.lock for Heroku

---
+++ rails_projects/first_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '47b9316ccbb8fe355cb17b3683134187d372515ee60f9a9f6657388a014b6aa490789705760058c72845f2bf86e4d9f0eaef099d6a9500a788d38e483feed04d'
